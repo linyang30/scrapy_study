e22e4c308c2ad69c71641d3d341f0cb1d45e1c3c
linyang <linyang30@163.com>
2016-12-21 17:41:21 +0800

    complete

--- zaobao_spider.py
+++ zaobao_spider.py
@@ -2,17 +2,17 @@ import scrapy
 
 class ZaobaoSpider(scrapy.Spider):
     name = 'zaobao'
-    start_urls = ['http://www.zaobao.com.sg/special/report/politic/fincrisis']
+    start_urls = ["http://stackoverflow.com/questions?sort=votes"]
 
     def parse(self, response):
-        for href in response.css('div.ds-1col a::attr(href)'):
+        for href in response.css('div.summary h3 a::attr(href)'):
             full_url = response.urljoin(href.extract())
             yield scrapy.Request(full_url, callback=self.parse_url)
 
     def parse_url(self, response):
         yield {
-            'title': response.css('div.body-content h1::text').extract(),
-            'dt': response.css('span.datestamp::text').extract(),
-            'body': response.css('div.article-content-container p::text').extract(),
+            'title': response.css('#question-header h1 a::text').extract(),
+            'dt': response.css('p.label-key b a::text').extract(),
+            'body': response.css('div.post-text p::text').extract(),
             'link': response.url
         }
